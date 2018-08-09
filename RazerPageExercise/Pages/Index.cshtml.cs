5b70c2536901c132c13ae4aef7ac6cbd3eaf1fd5
rcollantes <roberto.collantes@beeline.com>
2018-08-09 12:12:12 -0400

    final commit

--- RazerPageExercise/Pages/Index.cshtml.cs
+++ RazerPageExercise/Pages/Index.cshtml.cs
@@ -4,13 +4,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazerPageExercise.Data;
 
 namespace RazerPageExercise.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly AppDbContext db;
+
+        public IndexModel(AppDbContext db)
         {
+            this.db = db;
+        }
+
+        [BindProperty]
+        public IList<Customer> Customers { get; private set; }
+
+        public async Task OnGetAsync()
+        {
+            this.Customers = await this.db.Customers.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var contact = await this.db.Customers.FindAsync(id);
+
+            if (contact != null)
+            {
+                this.db.Customers.Remove(contact);
+                await this.db.SaveChangesAsync();
+            }
+
+            return RedirectToPage();
+
         }
     }
 }
